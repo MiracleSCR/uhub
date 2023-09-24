188d2b50e67b81aba115a6170889d89745d2b57d
MiracleSCR <145905541+MiracleSCR@users.noreply.github.com>
2023-09-24 16:05:24 +0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -16,11 +16,11 @@ local Window = Rayfield:CreateWindow({
    },
    KeySystem = true, -- Set this to true to use our key system
    KeySettings = {
-      Title = "Key | Youtube Hub",
-      Subtitle = "Key System",
-      Note = "Key In Discord Server",
-      FileName = "YoutubeHubKey1", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      Title = "🔑 Key System",
+      Subtitle = "Join Our Discord to Get 🔑",
+      Note = "🔑 In Discord Server\nJoin And Get 🔑",
+      FileName = "HubKey1", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
       Key = {"https://pastebin.com/raw/AtgzSPWK"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
